09c292d1be4adb85ca553b84544c8bd094013ba5
saddam-satria <ardhisaddam@gmail.com>
2023-02-21 12:59:32 +0700

    update final

--- dasarpemrogrammanweb/js/script.js
+++ dasarpemrogrammanweb/js/script.js
@@ -5,7 +5,7 @@ const hamburgerBtn = document.querySelector(".header-content .header-title .humb
 const modalScreen = document.querySelector('.modal-screen')
 const profileImage = document.querySelector(".main-content .profile-article .profile-image")
 
-
+const socialModal = document.querySelector(".main-content .profile-article .profile-social-content .profile-social-modal");
 
 hamburgerBtn.addEventListener('click', () => {
     navbar.classList.toggle('active')
@@ -18,4 +18,12 @@ profileImage.addEventListener("click", () => {
 
 modalScreen.addEventListener("click", () => {
     modalScreen.classList.remove("modal-active")
+})
+
+
+socialModal.addEventListener("mouseenter", () => {
+    socialModal.style.width = "80px"
+})
+socialModal.addEventListener("mouseleave", () => {
+    socialModal.style.width = "auto"
 })
